cb2c380b02d83526b28045d4e9add943cbd672d0
eliasmurcray <eliasmurcray@gmail.com>
2024-08-27 23:37:07 -0700

    Add fbm to noise module

--- include/noise.h
+++ include/noise.h
@@ -1,14 +1,14 @@
+#ifndef NOISE_H
+#define NOISE_H
+
+#include <stdlib.h>
 /**
  * Port of Java code for improved Perlin noise:
  * https://mrl.cs.nyu.edu/~perlin/noise/
  * Original author Ken Perlin
  **/
+float noise(float x, float y);
 
-#ifndef NOISE_H
-#define NOISE_H
-
-#include <stdlib.h>
-
-double noise(double x, double y, double z);
+float fbm(float x, float y, int octaves, float persistence);
 
 #endif
--- src/noise.c
+++ src/noise.c
@@ -1,6 +1,7 @@
 #include "noise.h"
-#include <stdio.h>
+
 #include <math.h>
+#include <stdio.h>
 
 static int p[512] = {
     151, 160, 137, 91,  90,  15,  131, 13,  201, 95,  96,  53,  194, 233, 7,
@@ -39,45 +40,47 @@ static int p[512] = {
     93,  222, 114, 67,  29,  24,  72,  243, 141, 128, 195, 78,  66,  215, 61,
     156, 180};
 
-static double fade(double t) {
+static float fade(float t) {
   return t * t * t * (t * (t * 6.0 - 15.0) + 10.0);
 }
 
-static double lerp(double t, double a, double b) { return a + t * (b - a); }
+static float lerp(float t, float a, float b) { return a + t * (b - a); }
 
-static double grad(int hash, double x, double y, double z) {
-  int h = hash & 15;
-  double u = h < 8 ? x : y;
-  double v = h < 4 ? y : (h == 12 || h == 14 ? x : z);
+static float grad(int hash, float x, float y) {
+  int h = hash & 7;
+  float u = h < 4 ? x : y;
+  float v = h < 4 ? y : x;
   return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
 }
 
-double noise(double x, double y, double z) {
+float noise(float x, float y) {
   int X = ((int)floor(x)) & 255;
   int Y = ((int)floor(y)) & 255;
-  int Z = ((int)floor(z)) & 255;
   x -= floor(x);
   y -= floor(y);
-  z -= floor(z);
 
-  double u = fade(x);
-  double v = fade(y);
-  double w = fade(z);
+  float u = fade(x);
+  float v = fade(y);
 
   int A = p[X] + Y;
-  int AA = p[A] + Z;
-  int AB = p[A + 1] + Z;
   int B = p[X + 1] + Y;
-  int BA = p[B] + Z;
-  int BB = p[B + 1] + Z;
 
-  return lerp(
-      w,
-      lerp(v, lerp(u, grad(p[AA], x, y, z), grad(p[BA], x - 1, y, z)),
-           lerp(u, grad(p[AB], x, y - 1, z), grad(p[BB], x - 1, y - 1, z))),
-      lerp(v,
-           lerp(u, grad(p[AA + 1], x, y, z - 1),
-                grad(p[BA + 1], x - 1, y, z - 1)),
-           lerp(u, grad(p[AB + 1], x, y - 1, z - 1),
-                grad(p[BB + 1], x - 1, y - 1, z - 1))));
+  return lerp(v, lerp(u, grad(p[A], x, y), grad(p[B], x - 1, y)),
+              lerp(u, grad(p[A + 1], x, y - 1), grad(p[B + 1], x - 1, y - 1)));
 }
+
+float fbm(float x, float y, int octaves, float persistence) {
+  float total = 0.0;
+  float frequency = 1.0;
+  float amplitude = 1.0;
+  float maxAmplitude = 0.0;
+  int i;
+  for (i = 0; i < octaves; i++) {
+    total += noise(x * frequency, y * frequency) * amplitude;
+    maxAmplitude += amplitude;
+    amplitude *= persistence;
+    frequency *= 2.0;
+  }
+  return total / maxAmplitude;
+}
+
